5b50eed8d86f9be154c37b37ecc4de6c17e15f88
mgy <73788063+mgy-qyqf@users.noreply.github.com>
2021-03-21 11:13:35 +0800

    Create HelloWord.cs

---
+++ HelloWord.cs
@@ -0,0 +1,12 @@
+using System;  //Using 关键字, System命名空间！
+namespace HelloWorldApplication //namespace声明命名空间,包含一个helloworld的类！
+{
+    class HelloWorld  
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
